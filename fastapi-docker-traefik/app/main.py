d097402349ae56684161521e09fb2ced0064d66a
Enrique García <86385297+enriquegv001@users.noreply.github.com>
2024-03-11 12:05:52 -0600

    Update main.py

--- fastapi-docker-traefik/app/main.py
+++ fastapi-docker-traefik/app/main.py
@@ -7,6 +7,13 @@ app = FastAPI(title="FastAPI, Docker, and Traefik")
 async def read_root():
     return await User.objects.all()
 
+@app.post("/users/")
+async def create_user(user: UserBase):
+    db_user = await User.objects.get_or_create(email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return db_user
+    
 @app.on_event("startup") # Create database connection
 async def startup():
     if not database.is_connected:
